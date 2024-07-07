d417b12dffd19fa93a9ebe0178a8a948dfabf660
Samuel Casco <samuelcasco44@gmail.com>
2024-07-07 13:24:28 -0600

    chore: Add new settings and packages

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,11 +4,12 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
+import { provideNativeDateAdapter } from '@angular/material/core';
 
 export const appConfig: ApplicationConfig = {
    providers: [
       provideRouter(routes),
-      provideAnimationsAsync(),
+      provideNativeDateAdapter(),
       provideAnimationsAsync(),
       {
          provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
