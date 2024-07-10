6b54ee9fc7baf6219370b32ff6b1af3bb921d6f3
Samuel Casco <samuelcasco44@gmail.com>
2024-07-09 22:40:12 -0600

    feat: Swiper added for pokemon preview

--- src/app/profile/components/preview-pokemons/preview-pokemons.component.ts
+++ src/app/profile/components/preview-pokemons/preview-pokemons.component.ts
@@ -1,10 +1,19 @@
-import { Component, inject, input, OnInit } from '@angular/core';
+import {
+   ChangeDetectionStrategy,
+   Component,
+   CUSTOM_ELEMENTS_SCHEMA,
+   inject,
+   input,
+   OnInit,
+} from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { PreviewPokemonCardComponent } from '../preview-pokemon-card/preview-pokemon-card.component';
 import { UserService } from '@shared/services/user/user.service';
 import { Router } from '@angular/router';
 import { PokemonI } from '@shared/interfaces/pokemon.interface';
+import { register } from 'swiper/element/bundle';
+register();
 
 @Component({
    selector: 'app-preview-pokemons',
@@ -12,6 +21,8 @@ import { PokemonI } from '@shared/interfaces/pokemon.interface';
    templateUrl: './preview-pokemons.component.html',
    styleUrl: './preview-pokemons.component.scss',
    imports: [MatIconModule, MatButtonModule, PreviewPokemonCardComponent],
+   schemas: [CUSTOM_ELEMENTS_SCHEMA],
+   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PreviewPokemonsComponent implements OnInit {
    userService = inject(UserService);
